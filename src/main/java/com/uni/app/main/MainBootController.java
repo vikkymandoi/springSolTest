8095259c95c6bb637a69ffb2559332fd8df4c092
vikkymandloi <imvrajendra@gmail.com>
2018-10-28 13:15:26 +0100

    First Commit

---
+++ src/main/java/com/uni/app/main/MainBootController.java
@@ -0,0 +1,11 @@
+package com.uni.app.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBootController {
+	public static void main(String[] args) {
+		SpringApplication.run(MainBootControllerTest.class, args);
+	}
+}
